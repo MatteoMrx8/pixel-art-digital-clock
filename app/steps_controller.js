8a9374097c8c4748ddc48d83b1b5c01adabe5a9a
Matteo Santarcangelo <matteomrx8@hotmail.it>
2022-02-14 11:42:39 +0100

    es6 last fix

--- app/steps_controller.js
+++ app/steps_controller.js
@@ -4,41 +4,44 @@ import { today } from 'user-activity';
 import clock from "clock";
 import { FitFont } from 'fitfont';
 
-export default function steps_controller() {
-    if (!appbit.permissions.granted("access_activity")) {
-        throw Exception('Activity permission required!');
-    }
-    
-    const steps_element = new FitFont({
-        id: 'steps-counter', // id of your symbol in the index.gui, you can also give an element object e.g. id: document.getElementById('foo')
-        font: 'Fixedsys_Excelsior_3_01_20', // name of the generated font folder
+export default class steps_controller {
 
-        // Optional
-        halign: 'middle', // horizontal alignment : start / middle / end
-        valign: 'baseline', // vertical alignment   : baseline / top / middle / bottom
-        letterspacing: 0            // letterspacing...
-    });
-    
-    const left_arm = document.getElementById('omino-arm-left');
-    const right_arm = document.getElementById('omino-arm-right');
-    const left_leg = document.getElementById('omino-leg-left');
-    const right_leg = document.getElementById('omino-leg-right');
-    
-    var last_steps = 0;
-    
-    clock.granularity = "seconds";
-    
-    // Update clock element every tick with the current time
-    clock.addEventListener('tick', (evt) => {
-        steps_element.text = today.adjusted.steps + ' s';
-        
-        if (last_steps != today.adjusted.steps) {
-            last_steps = today.adjusted.steps;
-            
-            left_arm.animate('enable');
-            right_arm.animate('enable');
-            left_leg.animate('enable');
-            right_leg.animate('enable');
+    last_steps = 0;
+
+    constructor() {
+        if (!appbit.permissions.granted("access_activity")) {
+            throw Exception('Activity permission required!');
         }
-    });
+
+        this.steps_element = new FitFont({
+            id: 'steps-counter', // id of your symbol in the index.gui, you can also give an element object e.g. id: document.getElementById('foo')
+            font: 'Fixedsys_Excelsior_3_01_20', // name of the generated font folder
+
+            // Optional
+            halign: 'middle', // horizontal alignment : start / middle / end
+            valign: 'baseline', // vertical alignment   : baseline / top / middle / bottom
+            letterspacing: 0            // letterspacing...
+        });
+
+        this.left_arm = document.getElementById('omino-arm-left');
+        this.right_arm = document.getElementById('omino-arm-right');
+        this.left_leg = document.getElementById('omino-leg-left');
+        this.right_leg = document.getElementById('omino-leg-right');
+
+        clock.granularity = "seconds";
+        
+        // Update clock element every tick with the current time
+        clock.addEventListener('tick', (evt) => {
+            this.steps_element.text = today.adjusted.steps + ' s';
+
+            if (this.last_steps != today.adjusted.steps) {
+                this.last_steps = today.adjusted.steps;
+
+                this.left_arm.animate('enable');
+                this.right_arm.animate('enable');
+                this.left_leg.animate('enable');
+                this.right_leg.animate('enable');
+            }
+        });
+    }
 }
